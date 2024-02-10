4217ec35d20d164b8f164fd5dd4ae9a7e049be2b
xiongdian.me <xiongdian.me@bytedance.com>
2024-02-11 00:44:23 +0800

    fix str format

--- cf-client.py
+++ cf-client.py
@@ -51,7 +51,7 @@ class DNSRecord:
         self.id = id
 
     def __str__(self):
-        return f'{self.name:28} IN {self.type:5} {self.content:39} {self.ttl}'
+        return f'{self.name:28} {self.ttl:5}  IN  {self.type:5} {self.content:39}'
 
     def __eq__(self, __value: object) -> bool:
         if not isinstance(__value, DNSRecord):
