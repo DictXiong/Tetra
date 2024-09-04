851e59419a6472cac73ac622aae17926b2e1613f
Dict Xiong <me@beardic.cn>
2024-09-04 18:01:31 +0800

    fix(cloudflare): paging issue

--- tetra/backends/cloudflare.py
+++ tetra/backends/cloudflare.py
@@ -7,8 +7,8 @@ class CloudflareClient:
     def __init__(self, domain: str, auth: dict, prefix: str):
         self.domain = domain
         self.prefix = prefix
-        self.cf = CloudFlare.CloudFlare(token=auth['token'])
-        zones = self.cf.zones.get(params={'name': domain})
+        self.cf = CloudFlare.CloudFlare(token=auth['token'], raw=True)
+        zones = self.cf.zones.get(params={'name': domain})['result']
         if len(zones) != 1:
             raise ValueError(f"Zone {domain} not found")
         self.zone_id = zones[0]['id']
@@ -16,7 +16,13 @@ class CloudflareClient:
     
     def get_records(self):
         logging.debug(f"Getting records for domain {self.domain}")
-        dns_records = self.cf.zones.dns_records.get(self.zone_id)
+        dns_records = []
+        page = 1
+        while True:
+            tmp = self.cf.zones.dns_records.get(self.zone_id, params={'per_page': 1000, 'page': page})
+            dns_records += tmp['result']
+            if tmp['result_info']['total_pages'] == page: break
+            else: page += 1
         cf_records = []
         for record in dns_records:
             if record['comment'] and self.prefix in record['comment']:
